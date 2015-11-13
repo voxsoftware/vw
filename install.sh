bd772dc81ef7b09b63220ce68e5cf2394808ef6d
james <xtreme.maicolxt94@gmail.com>
2015-11-12 19:04:42 -0500

    Ajustes en el instalador

--- install.sh
+++ install.sh
@@ -34,16 +34,16 @@ fi
 ## Copiando los binarios ...
 sudo cp ./vw "$INSTALL_DIR/vw"
 
-sudo cp /usr/local/bin/jx /"$INSTALL_DIR/vwdata/jx"
+sudo cp /usr/local/bin/jx "$INSTALL_DIR/vwdata/jx"
 # Esto es para copiar el exe jx al directorio establecido ...
 #sudo jx "$DIR/install.js" "$INSTALL_DIR"
 
-
+JX= "$INSTALL_DIR/vwdata/jx"
 cd "$INSTALL_DIR/vwdata"
-sudo jx install vw-server # instala vw-server 
-sudo jx install vw ## Instala el módulo VW con sus dependencias ...
-sudo jx install nw ## Instala el módulo NW con sus dependencias ...
-sudo jx install mime-types
-sudo jx install app-module-path
+sudo "$JX" install vw-server # instala vw-server 
+sudo "$JX" install vw ## Instala el módulo VW con sus dependencias ...
+sudo "$JX" install nw ## Instala el módulo NW con sus dependencias ...
+sudo "$JX" install mime-types
+sudo "$JX" install app-module-path
 
 ## Hasta acá ya está listo para usar ...
